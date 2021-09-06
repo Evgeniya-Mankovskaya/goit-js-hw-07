4de3f8a25f3a0db6e19a160ac5f6f0863d56fe9b
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-09-06 17:55:18 +0300

    Update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,14 @@
 const inpAr = document.querySelector("#validation-input");
 const dataLengthEl = inpAr.getAttribute("data-length");
 inpAr.addEventListener("blur", inpFunc);
+function valInval(valid, invalid) {
+  return valid, invalid;
+}
+
 function inpFunc() {
   if (inpAr.value.length === Number(dataLengthEl)) {
-    inpAr.classList.remove("invalid");
-    inpAr.classList.add("valid");
+    valInval(inpAr.classList.remove("invalid"), inpAr.classList.add("valid"));
   } else {
-    inpAr.classList.remove("valid");
-    inpAr.classList.add("invalid");
+    valInval(inpAr.classList.remove("valid"), inpAr.classList.add("invalid"));
   }
 }
