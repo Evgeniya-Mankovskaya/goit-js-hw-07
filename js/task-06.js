2ef83cdb1ca95940449ee9c26071ce336089c90d
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-09-07 19:29:22 +0300

    update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,29 @@
+// const inpAr = document.querySelector("#validation-input");
+// const dataLengthEl = inpAr.getAttribute("data-length");
+// inpAr.addEventListener("blur", inpFunc);
+// function valInval(valid, invalid) {
+//   return valid, invalid;
+// }
+
+// function inpFunc() {
+//   if (inpAr.value.length === Number(dataLengthEl)) {
+//     valInval(inpAr.classList.remove("invalid"), inpAr.classList.add("valid"));
+//   } else {
+//     valInval(inpAr.classList.remove("valid"), inpAr.classList.add("invalid"));
+//   }
+// }
 const inpAr = document.querySelector("#validation-input");
 const dataLengthEl = inpAr.getAttribute("data-length");
 inpAr.addEventListener("blur", inpFunc);
 function valInval(valid, invalid) {
-  return valid, invalid;
+  inpAr.classList.add(valid);
+  inpAr.classList.remove(invalid);
 }
 
 function inpFunc() {
   if (inpAr.value.length === Number(dataLengthEl)) {
-    valInval(inpAr.classList.remove("invalid"), inpAr.classList.add("valid"));
+    valInval("valid", "invalid");
   } else {
-    valInval(inpAr.classList.remove("valid"), inpAr.classList.add("invalid"));
+    valInval("invalid", "valid");
   }
 }
